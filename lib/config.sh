d150a69cb3d735818def65b0b541f7893302b19b
Alonso <alonso.garcia@iesdonana.org>
2020-09-08 15:28:11 +0200

    add(extensions): agregados modulos de extensiones de gnome

--- lib/config.sh
+++ lib/config.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 source <(curl -sLo- "https://git.io/Je0t7")
-
-NULL='/dev/null'
-
-CURRENT_OS_VERSION=$(lsb_release -r -s)
-
-REQUIRED_OS_VERSION='20.04'
-
-EXTENSION_NAMESPACE_SCREENSHOT='gnome-shell-screenshot@ttll.de'
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 BASE_DIR=$(dirname "$(readlink -f "$0")")
 
 echo -e 'Inicializando, por favor, espere ...'
+source $BASE_DIR/lib/params.sh
 source $BASE_DIR/lib/config.sh
 source $BASE_DIR/lib/helper.sh
 source $BASE_DIR/scripts/packages.sh
