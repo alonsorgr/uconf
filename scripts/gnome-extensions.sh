97074f8d1f564b5d312a4d63292a11469e927599
Alonso <alonso.garcia@iesdonana.org>
2020-09-10 09:38:54 +0200

    fix(): orden de instalación de extensiones

--- scripts/gnome-extensions.sh
+++ scripts/gnome-extensions.sh
@@ -23,23 +23,23 @@ NAMESPACE_SRC_USER_THEMES='gnome-shell-extensions/extensions/user-theme'
 
 message 'Instalación de extensiones de GNOME'
 
+call_command "make -C ${SRC_PREFIX}/${NAMESPACE_SRC_EASY_SCREEN_CAST}" "Configurándo extensión ${NAMESPACE_SRC_EASY_SCREEN_CAST}, por favor, espere ..." "Error al configurar la extensión ${NAMESPACE_SRC_EASY_SCREEN_CAST}"
+
+call_command "make -C ${SRC_PREFIX}/${NAMESPACE_SRC_SCREENSHOT_TOOL}" "Configurándo extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}, por favor, espere ..." "Error al configurar la extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}"
+
+call_command "make -C ${SRC_PREFIX}/${NAMESPACE_SRC_DASH_TO_PANEL}" "Configurándo extensión ${NAMESPACE_SRC_DASH_TO_PANEL}, por favor, espere ..." "Error al configurar la extensión ${NAMESPACE_SRC_DASH_TO_PANEL}"
+
 call_command "cp -r ${SRC_PREFIX}/${NAMESPACE_SRC_CLIPBOARD_INDICATOR}/* ${DST_PREFIX}/${NAMESPACE_DST_CLIPBOARD_INDICATOR}" "Instalando extensión ${NAMESPACE_SRC_CLIPBOARD_INDICATOR}, por favor, espere ..." "Error al instalar la extensión ${NAMESPACE_SRC_CLIPBOARD_INDICATOR}"
+
+call_command "cp -r ${SRC_PREFIX}/${NAMESPACE_SRC_SIMPLE_OFF_MENU}/* ${DST_PREFIX}/${NAMESPACE_DST_SIMPLE_OFF_MENU}" "Instalando extensión ${NAMESPACE_SRC_SIMPLE_OFF_MENU}, por favor, espere ..." "Error al instalar la extensión ${NAMESPACE_SRC_SIMPLE_OFF_MENU}"
+
+call_command "cp -r ${SRC_PREFIX}/${NAMESPACE_SRC_USER_THEMES} ${DST_PREFIX}/${NAMESPACE_DST_USER_THEMES}" "Instalando extensión ${NAMESPACE_SRC_USER_THEMES}, por favor, espere ..." "Error al instalar la extensión ${NAMESPACE_SRC_USER_THEMES}"
 
 call_command "make install -C ${SRC_PREFIX}/${NAMESPACE_SRC_EASY_SCREEN_CAST}" "Instalando extensión ${NAMESPACE_SRC_EASY_SCREEN_CAST}, por favor, espere ..." "Error al instlar la extensión ${NAMESPACE_SRC_EASY_SCREEN_CAST}"
 
 call_command "make install -C ${SRC_PREFIX}/${NAMESPACE_SRC_SCREENSHOT_TOOL}" "Instalando extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}, por favor, espere ..." "Error al instalar la extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}" && cp -r "${SRC_PREFIX}/${NAMESPACE_SRC_SCREENSHOT_TOOL}-config"/* "${DST_PREFIX}/${NAMESPACE_DST_SCREENSHOT_TOOL}"
 
 call_command "make install -C ${SRC_PREFIX}/${NAMESPACE_SRC_DASH_TO_PANEL}" "Instalando extensión ${NAMESPACE_SRC_DASH_TO_PANEL}, por favor, espere ..." "Error al instlar la extensión ${NAMESPACE_SRC_DASH_TO_PANEL}"
-
-call_command "cp -r ${SRC_PREFIX}/${NAMESPACE_SRC_SIMPLE_OFF_MENU}/* ${DST_PREFIX}/${NAMESPACE_DST_SIMPLE_OFF_MENU}" "Instalando extensión ${NAMESPACE_SRC_SIMPLE_OFF_MENU}, por favor, espere ..." "Error al instalar la extensión ${NAMESPACE_SRC_SIMPLE_OFF_MENU}"
-
-call_command "cp -r ${SRC_PREFIX}/${NAMESPACE_SRC_USER_THEMES} ${DST_PREFIX}/${NAMESPACE_DST_USER_THEMES}" "Instalando extensión ${NAMESPACE_SRC_USER_THEMES}, por favor, espere ..." "Error al instalar la extensión ${NAMESPACE_SRC_USER_THEMES}"
-
-call_command "make -C ${SRC_PREFIX}/${NAMESPACE_SRC_EASY_SCREEN_CAST}" "Configurándo extensión ${NAMESPACE_SRC_EASY_SCREEN_CAST}, por favor, espere ..." "Error al configurar la extensión ${NAMESPACE_SRC_EASY_SCREEN_CAST}"
-
-call_command "make -C ${SRC_PREFIX}/${NAMESPACE_SRC_SCREENSHOT_TOOL}" "Configurándo extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}, por favor, espere ..." "Error al configurar la extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}"
-
-call_command "make -C ${SRC_PREFIX}/${NAMESPACE_SRC_DASH_TO_PANEL}" "Configurándo extensión ${NAMESPACE_SRC_DASH_TO_PANEL}, por favor, espere ..." "Error al configurar la extensión ${NAMESPACE_SRC_DASH_TO_PANEL}"
 
 call_command "gnome-extensions enable ${NAMESPACE_DST_SCREENSHOT_TOOL}" "Inicializando extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}, por favor, espere ..." "Error al inicializar la extensión ${NAMESPACE_SRC_SCREENSHOT_TOOL}"
 
