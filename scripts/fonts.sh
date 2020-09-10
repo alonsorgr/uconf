47e90f2dd0b15cce49edf2517a21ad1e05f76991
Alonso <alonso.garcia@iesdonana.org>
2020-09-10 23:10:20 +0200

    feat(fonts): agregadas tipografías

---
+++ scripts/fonts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Title:Tipografías de programador
+
+SRC_FONTS="${BASE_DIR}/fonts"
+DST_FONTS="$HOME/.local/share/fonts"
+FONTS_LIST=$(cat ${BASE_DIR}/config/fonts.cfg)
+
+message 'Intsalación de Tipografías de programador'
+
+mkdir -p ${DST_FONTS}
+
+for font in ${FONTS_LIST}; do
+    execute "cp -rf ${SRC_FONTS}/$f/* ${FONTS_DIR}" "Instalándo tipografía ${font}, por favor, espere ..." "Error al instalar la tipografía ${font}"
+done
+
+execute "fc-cache -f ${FONTS_DIR}" "Actualizando caché de tipografías del sistema, por favor, espere ..."
+
+execute "${SRC_FONTS}/powerline-fonts/install.sh" "Instalándo tipografías Powerline Fonts, por favor, espere ..." "Error al instalar la tipografía Powerline Fonts"
+
+success_message 'Instalación de tipografías terminada\n'
