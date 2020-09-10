4b27299d0d53b6b769910c5f5f705e66cac713b6
Alonso <alonso.garcia@iesdonana.org>
2020-09-10 22:31:48 +0200

    fix(paquetes): optimización de instalación de paquetes

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -5,13 +5,10 @@
 REPOSITORIES_LIST=$(cat ${BASE_DIR}/config/repositories.cfg)
 REPOSITORIES_COUNT=$(echo ${REPOSITORIES_LIST} | wc -w)
 
-DEPENDENCES_LIST=$(cat ${BASE_DIR}/config/dependences.cfg)
-DEPENDENCES_COUNT=$(echo ${DEPENDENCES_LIST} | wc -w)
-
 PACKAGES_LIST=$(cat ${BASE_DIR}/config/packages.cfg)
 PACKAGES_COUNT=$(echo ${PACKAGES_LIST} | wc -w)
 
-COUNT=$((${DEPENDENCES_COUNT} + ${PACKAGES_COUNT} + ${REPOSITORIES_COUNT}))
+COUNT=$((${REPOSITORIES_COUNT} + ${PACKAGES_COUNT}))
 
 message 'Actualizando paquetes instalados en el sistema'
 
@@ -22,15 +19,10 @@ success_message 'Actualización de de paquetes terminada\n'
 message 'Iniciando instalación de paquetes'
 
 bar::start
-for repositories in ${REPOSITORIES_LIST}; do
-    enable_repository ${repositories}
+for repository in ${REPOSITORIES_LIST}; do
+    enable_repository ${repository}
     steps_done_repositories=$((${steps_done_repositories:-0}+1))
     bar::status_changed ${steps_done_repositories} ${COUNT}
-done
-for dependence in ${DEPENDENCES_LIST}; do
-    install_package ${dependence}
-    steps_done_dependence=$((${steps_done_dependence:-0}+1))
-    bar::status_changed ${steps_done_dependence} ${COUNT}
 done
 for package in ${PACKAGES_LIST}; do
     install_package ${package}
