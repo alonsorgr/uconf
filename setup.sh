df104996bc99b4143c69f7811d0541aea1c63a63
Alonso <alonso.garcia@iesdonana.org>
2020-09-14 10:38:23 +0200

    fix(): lista de scripts

--- setup.sh
+++ setup.sh
@@ -56,8 +56,7 @@ success_message "Intslación de dependencia $(package_info "curl") terminada\n"
 
 source ${BASE_DIR}/lib/sources.sh
 
-#SCRIPTS='packages drivers gnome-extensions theme icons wayland fonts dconf sakura oh-my-zsh'
-SCRIPTS='gtk'
+SCRIPTS='packages drivers gnome-extensions theme icons wayland fonts dconf sakura oh-my-zsh gtk'
 
 module_update
 success_message 'Actualización de módulos terminada\n'
