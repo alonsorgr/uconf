92cac6796d49373d02ec3fa1f457dea0e85b0a55
Alonso <alonsorgr@gmail.com>
2024-10-18 15:58:04 +0200

    feat: gtk and plymouth theme settings

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -6,35 +6,23 @@
 # @license https://raw.githubusercontent.com/alonsorgr/uconf/master/LICENSE?token=AH3YUC7WYRDYPH26XTVMTXK7NHANA
 ##
 
-# Name:Tema GTK Plata
+# Name:Tema Material Monokai Metallian
 
-message "Eliminando versiones anteriores del tema GTK Plata, espere ..."
-run sudo rm -rf /usr/share/themes/{Plata,Plata-Compact,Plata-Lumine,Plata-Lumine-Compact,Plata-Noir,Plata-Noir-Compact}
-errors "Error al eliminar las versiones anteriores del tema GTK Plata"
-
-message "Configurando tema GTK plata, espere ..."
-run "${__DIR__}/theme/plata-theme/autogen.sh --prefix=/usr" --with-selection_color=${SELECTION_COLOR} --with-accent_color=${ACCENT_COLOR} --with-suggestion_color=${SUGGESTION_COLOR}
+message "Configurando e instalando tema GTK Material Monokai Metallian, espere ..."
+run "${__DIR__}/theme/material-monokai-metallian/install.sh -n Material-Monokai-Metallian -t grey -c dark -l --size standard --tweaks submenu primary solid"
 errors "Error al configurar el tema GTK Plata"
 
-message "Generando tema GTK plata configurado, espere ..."
-run sudo make -C "${__DIR__}/theme/plata-theme"
-errors "Error al generar el tema GTK Plata"
+message "Activando tema GTK Material-Monokai-Metallian generado, espere ..."
+run gsettings set org.gnome.desktop.interface gtk-theme 'Material-Monokai-Metallian-Grey-Dark'
+errors "Error al activar el tema GTK Material-Monokai-Metallian"
 
-message "Instalando tema GTK plata generado, espere ..."
-run sudo make install -C "${__DIR__}/theme/plata-theme"
-errors "Error al instalar el tema GTK Plata"
+message "Activando tema GTK Material-Monokai-Metallian generado en pantalla de inicio de sesión, espere ..."
+run gsettings set org.gnome.shell.extensions.user-theme name 'Material-Monokai-Metallian-Grey-Dark'
+errors "Error al activar el tema GTK Material-Monokai-Metallian en pantalla de inicio de sesión"
 
-message "Activando tema GTK plata generado, espere ..."
-run gsettings set org.gnome.desktop.interface gtk-theme 'Plata-Noir'
-errors "Error al activar el tema GTK Plata"
-
-message "Generando tema GTK plata para pantalla de inicio de sesión, espere ..."
-run sudo "${__DIR__}/scripts/gdm.sh"
-errors "Error al generar el tema GTK plata de pantalla de inicio de sesión"
-
-message "Activando tema GTK plata generado en pantalla de inicio de sesión, espere ..."
-run gsettings set org.gnome.shell.extensions.user-theme name 'Plata-Noir'
-errors "Error al activar el tema GTK plata en pantalla de inicio de sesión"
+message "Estableciéndo configuración del logo de la pantalla de inicio de sesión, espere ..."
+run sudo cp -rf "${__DIR__}/config/ubuntu-logo.png" "/usr/share/plymouth/ubuntu-logo.png"
+errors "Error al establecer la configuración del logo de la pantalla de inicio de sesión"
 
 message "Estableciéndo el fondo de escritorio, espere ..."
 run gsettings set org.gnome.desktop.background picture-uri "file:///home/${USER}/.uconf/config/background.jpg"
@@ -43,3 +31,11 @@ errors "Error al establecer el fondo de escritorio"
 message "Estableciéndo el fondo de salva pantallas, espere ..."
 run gsettings set org.gnome.desktop.screensaver picture-uri "file:///home/${USER}/.uconf/config/screensaver.jpg"
 errors "Error al establecer el fondo de salva pantallas"
+
+message "Estableciéndo configuración de Plymouth, espere ..."
+run sudo cp -rf "${__DIR__}/config/watermark.png" "/usr/share/plymouth/themes/spinner/watermark.png"
+errors "Error al establecer la configuración de Plymouth"
+
+message "Estableciéndo configuración del logo de Plymouth, espere ..."
+run sudo cp -rf "${__DIR__}/config/ubuntu-logo.png" "/usr/share/plymouth/themes/spinner/bgrt-fallback.png"
+errors "Error al establecer la configuración del logo de Plymouth"
