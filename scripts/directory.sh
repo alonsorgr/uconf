fcadc7bbd6be07ca4a948401e411c3890cc086ad
& <&>
2024-10-24 13:50:33 +0200

    fix: directory icons

--- scripts/directory.sh
+++ scripts/directory.sh
@@ -8,16 +8,10 @@
 
 # Name:Configuración de directorios del usuario
 
-message "Creando directorio Web en el directorio personal, espere ..."
-if [ ! -d "${HOME}/Web" ]; then
-    mkdir "${HOME}/Web"
-    run gio set "${HOME}/Web" metadata::custom-icon 'file:///usr/share/icons/Papirus/64x64/places/folder-white-code.svg'
-    errors "Error al crear el directorio 'Web' en el directorio personal"
-fi
-
 message "Creando directorio Repositorios en el directorio personal, espere ..."
 if [ ! -d "${HOME}/Repositorios" ]; then
     mkdir "${HOME}/Repositorios"
     run gio set "${HOME}/Repositorios" metadata::custom-icon 'file:///usr/share/icons/Papirus/64x64/places/folder-white-github.svg'
+    run gvfs-set-attribute "${HOME}/Repositorios" -t string metadata::custom-icon 'file:///usr/share/icons/Papirus/64x64/places/folder-white-github.svg'
     errors "Error al crear el directorio Web en el directorio personal"
 fi
